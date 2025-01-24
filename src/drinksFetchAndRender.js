9733aad84ae960566f24b5882750fc70d850ecc4
emmeiwhite <emmeiwhite@gmail.com>
2025-01-25 01:00:45 +0530

    async-1.7 an async function always returns a promise! no matter what

--- src/drinksFetchAndRender.js
+++ src/drinksFetchAndRender.js
@@ -1,9 +1,12 @@
 // To fetch drinks & display drinks
+import fetchDrinks from './fetchDrinks.js'
+
 async function fetchAndRender(url) {
   console.log(url)
   // 1. Fetch Drinks: We make another function call to fetch drinks
-
+  const data = await fetchDrinks(url)
+  console.log(data)
   // 2. Display drinks: Combine Data with UI Template
 }
 
-export default showDrinks
+export default fetchAndRender
--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -1,1 +1,6 @@
+// An async-function always returns a promise (no matter what)
+const fetchDrinks = async url => {
+  return 1
+}
 
+export default fetchDrinks
