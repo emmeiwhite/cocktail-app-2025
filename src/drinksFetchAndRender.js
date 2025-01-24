dc3748c1df999c2552d5ae71d59ac03d5b8ca0f0
emmeiwhite <emmeiwhite@gmail.com>
2025-01-25 01:05:03 +0530

    async-1.8 data fetched from an API successfully

--- src/drinksFetchAndRender.js
+++ src/drinksFetchAndRender.js
@@ -5,7 +5,7 @@ async function fetchAndRender(url) {
   console.log(url)
   // 1. Fetch Drinks: We make another function call to fetch drinks
   const data = await fetchDrinks(url)
-  console.log(data)
+
   // 2. Display drinks: Combine Data with UI Template
 }
 
--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -1,6 +1,13 @@
 // An async-function always returns a promise (no matter what)
 const fetchDrinks = async url => {
-  return 1
+  try {
+    const response = await fetch(url)
+    const data = response.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default fetchDrinks
