86cb3583cd02d00c7851c2b58c10ef28c14ab5b0
emmeiwhite <emmeiwhite@gmail.com>
2025-01-24 20:37:40 +0530

    async-1.2 Imperative approach to create project

---
+++ app.js
@@ -0,0 +1,5 @@
+// Modules are executed in strict mode by default
+// Each module has its own scope so that variables and functions are not added to the global scope
+// We can import other modules into this file
+
+const URL = `www.thecocktaildb.com/api/json/v1/1/search.php?s=a`
--- src/toggleLoading.js
+++ src/toggleLoading.js
@@ -1,1 +1,1 @@
-const URL = `www.thecocktaildb.com/api/json/v1/1/search.php?s=a`
+
