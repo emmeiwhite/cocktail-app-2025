fd963f1254f2ba461aacb2c691b6c3bd7ff5df18
emmeiwhite <emmeiwhite@gmail.com>
2025-01-25 00:45:43 +0530

    async-1.5 function created to fetch & display drinks

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
 // Each module has its own scope so that variables and functions are not added to the global scope
 // We can import other modules into this file
 
-const URL = `www.thecocktaildb.com/api/json/v1/1/search.php?s=a`
+const url = `www.thecocktaildb.com/api/json/v1/1/search.php?s=a`
+
+import presentDrinks from './src/presentDrinks.js'
+
+// Once the page loads, we'll call our presentDrinks function to fetch & render the drinks
+window.addEventListener('DOMContentLoaded', function () {
+  presentDrinks(url)
+})
--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,1 +1,9 @@
+// To fetch drinks & display drinks
+async function showDrinks(url) {
+  console.log(url)
 
+  // fetch drinks
+  // display drinks
+}
+
+export default showDrinks
