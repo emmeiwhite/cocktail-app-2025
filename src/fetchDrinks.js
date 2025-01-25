a0de47b014d12f1b6c172fb6ba48db3a7df5ed23
emmeiwhite <emmeiwhite@gmail.com>
2025-01-25 23:50:43 +0530

    async-2.2 cleaned loading code!

--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -1,20 +1,19 @@
-import get from './getElement.js'
-// An async-function always returns a promise (no matter what)
-const loaderDiv = get('.loading')
+import { showLoading, hideLoading } from './toggleLoading.js'
+
 const fetchDrinks = async url => {
+  showLoading()
   try {
-    loaderDiv.classList.remove('hide-loading')
     const response = await fetch(url)
     const data = await response.json()
     if (data) {
-      loaderDiv.classList.add('hide-loading')
+      hideLoading()
     } else {
       throw new Error('Error fetching data')
     }
     console.log(data)
     return data // fetchDrinks either returns data or returns null from the API
   } catch (error) {
-    loaderDiv.classList.add('hide-loading')
+    hideLoading()
     console.log(error)
   }
 }
--- src/toggleLoading.js
+++ src/toggleLoading.js
@@ -1,1 +1,13 @@
+import get from './getElement.js'
+// An async-function always returns a promise (no matter what)
+const loaderDiv = get('.loading')
 
+const showLoading = () => {
+  loaderDiv.classList.remove('hide-loading')
+}
+
+const hideLoading = () => {
+  loaderDiv.classList.add('hide-loading')
+}
+
+export { showLoading, hideLoading }
