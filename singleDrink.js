d6da0c68126ec84335e9a4ebccaa1f2f68436715
emmeiwhite <emmeiwhite@gmail.com>
2025-01-26 17:33:05 +0530

    async-2.4 localStorage to store id, drink.html page structure

--- singleDrink.js
+++ singleDrink.js
@@ -1,1 +1,27 @@
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007'
+
+import get from './src/getElement.js'
+
+/*
+const grantParent = get('.grand-parent')
+const parent = get('.parent')
+const child = get('.child')
+
+grantParent.addEventListener('click', function (e) {
+  console.log('Grant Parent Event Handled')
+})
+
+parent.addEventListener(
+  'click',
+  function (e) {
+    console.log('Parent Event Handled')
+  },
+  { capture: true }
+)
+
+child.addEventListener('click', function (e) {
+  console.log('Child Event Handled')
+})
+  */
+
+// Target element will be child, Let's see the behavior
--- src/setDrink.js
+++ src/setDrink.js
@@ -3,9 +3,14 @@ export default function setDrink(section) {
 
   // Event Delegation: Instead of attaching an event handler to each child element individually, you can add a single event listener to a common ancestor. This way when an event occurs on a child, it bubbles up to the ancestor, where it can be handled centrally.
   section.addEventListener('click', function (e) {
-    e.preventDefault()
+    // e.preventDefault()
     //   Either the user clicks on the the image or the h3 element and we need to get the id. Both the image and the h3 have a common parent. e.target.parentElement.dataset.id will do it :)
     const id = e.target.parentElement.dataset.id
     console.log(id)
+
+    //   But practically speaking we have to move to a different page drink.html. Now the question arises: How to store our id :)
+
+    // localStorage to the rescue
+    localStorage.setItem('drink-id', id)
   })
 }
