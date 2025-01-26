7d8a8eaa3b3aba9328684cec52c5a8478f794bf8
emmeiwhite <emmeiwhite@gmail.com>
2025-01-26 19:15:40 +0530

    async-2.8 project is ready with good learnings and revisions

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -1,21 +1,41 @@
 import get from './getElement.js'
 
-export default function displaySinceDrink(drink) {
-  const drinkData = drink.drinks[0]
+export default function displaySinceDrink(data) {
+  const drinkData = data.drinks[0]
   console.log(drinkData)
 
   const drinkSection = get('.single-drink')
-  const { strDrinkThumb: drinkImage, strDrink: drinkName } = drinkData
+  const { strDrinkThumb: image, strDrink: name, strInstructions: desc } = drinkData
+  const { strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5 } =
+    drinkData
+
+  // saving ingredients in an array to handle these wisely
+  const ingredients = [
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5
+  ]
+
+  const finalIngredients = ingredients
+    .map(ingredient => {
+      if (!ingredient) return
+      return `<li><i class="far fa-check-square"></i>${ingredient}</li>`
+    })
+    .join('')
 
   const UIwithData = `
       <img
-        src="${drinkImage}"
+        src="${image}"
         alt="cocktail drink"
         class="drink-img" />
       <article class="drink-info">
-        <h2 class="drink-name">${drinkName}</h2>
-        <p class="drink-desc"></p>
-        <ul class="drink-ingredients"></ul>
+        <h2 class="drink-name">${name}</h2>
+        <p class="drink-desc">${desc}</p>
+        <ul class="drink-ingredients">
+          ${finalIngredients}
+        </ul>
         <a
           href="index.html"
           class="btn">
